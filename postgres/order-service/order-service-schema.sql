0a8f93168a7af6b09eb356e232b157d115eafe9b
gianluca farinaccio <gianluca.farinaccio@gmail.com>
2024-01-04 15:18:57 +0100

    fix: initialization files for postgres db

--- postgres/order-service/order-service-schema.sql
+++ postgres/order-service/order-service-schema.sql
@@ -1,2 +1,3 @@
-SELECT 'CREATE DATABASE orderservice'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'orderservice')\gexec
+-- SELECT 'CREATE DATABASE orderservice'
+-- WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'orderservice')\gexec
+create database orderservice;
